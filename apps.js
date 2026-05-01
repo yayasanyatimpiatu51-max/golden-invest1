e2f94bd0ce097a6e21b2fc8fe57ac9b36af9fd8b
yayasanyatimpiatu51-max <yayasanyatimpiatu51@gmail.com>
2026-05-01 07:19:32 +0700

    Update apps.js

--- apps.js
+++ apps.js
@@ -1,11 +1,10 @@
 import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
 
 export const supabase = createClient(
-  "https://fqqhekmxkrwatfhephpr.supabase.co",
-  "PASTE_ANON_KEY_KAMU"
+  "https://SUPABASE_URL.supabase.co",
+  "SUPABASE_ANON_KEY"
 );
 
-// 🔥 AUTO USER (sementara)
 export function getUser(){
   let u = localStorage.getItem("user");
   if(!u){
