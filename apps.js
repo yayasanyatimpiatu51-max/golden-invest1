8e2d3dfc899493da7bd5aa2f74fac83374f9721e
yayasanyatimpiatu51-max <yayasanyatimpiatu51@gmail.com>
2026-05-01 07:28:52 +0700

    Update apps.js

--- apps.js
+++ apps.js
@@ -2,7 +2,7 @@ import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js
 
 export const supabase = createClient(
   "https://SUPABASE_URL.supabase.co",
-  "SUPABASE_ANON_KEY"
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZxcWhla214a3J3YXRmaGVwaHByIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzc1NjQ1NDYsImV4cCI6MjA5MzE0MDU0Nn0.-QXKtJjVs36IyYCq0YiWfJuOw7WKS9iHkNG1Ewhwq-o"
 );
 
 export function getUser(){
